512bea2f7d07647f05d4729d498eb1dc1e110878
Irlirion <43699715+Irlirion@users.noreply.github.com>
2019-02-18 21:07:25 +0300

    Добавил корабль на экран

--- ship.py
+++ ship.py
@@ -11,10 +11,9 @@ class Ship():
         self.rect = self.image.get_rect()
         self.screen_rect = screen.get_rect()
 
-        # Каждый новфй корабль появляется у нижнего края экрана
-        self.rect.conterx = self.screen_rect.conterx
+        # Каждый новый корабль появляется у нижнего края экрана
+        self.rect.centerx = self.screen_rect.centerx
         self.rect.bottom = self.screen_rect.bottom
-
     def blitme(self):
         """Рисует корабль в текущей позиции"""
         self.screen.blit(self.image, self.rect)
