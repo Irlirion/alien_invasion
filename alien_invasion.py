e00b7cb09d8e8c8e82cefd75deb76b0b961c96b4
Irlirion <43699715+Irlirion@users.noreply.github.com>
2019-02-18 19:54:23 +0300

    Изменил цвет фона на серый

--- alien_invasion.py
+++ alien_invasion.py
@@ -10,12 +10,18 @@ def run_game():
     screen = pygame.display.set_mode((1200, 800))
     pygame.display.set_caption("Alien Invasion")
 
+    # Назанчение цвета фона
+    bg_color = (230, 230, 230)
+
     # Запуск цикла игры
     while True:
         # Отслеживание событий клавиатуры
         for even in pygame.event.get():
             if even.type == pygame.QUIT:
                 sys.exit()
+
+        # ПРи каждом проходе цикла перерисовывается экран
+        screen.fill(bg_color)
 
         # Отображение последнего прорисованного экрана
         pygame.display.flip()
