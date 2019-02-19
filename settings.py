a6a4a6d9f1bcffe96b126602e5d77fde73fa05c6
Irlirion <43699715+Irlirion@users.noreply.github.com>
2019-02-19 19:48:25 +0300

    Добавил параметры пули

--- settings.py
+++ settings.py
@@ -6,7 +6,13 @@ class Settings:
         # Параметры экрана
         self.screen_width = 1200
         self.screen_height = 800
-        self.bg_color = (230, 230, 230)
+        self.bg_color = 230, 230, 230
+
+        # Параметры пули
+        self.bullet_speed_factor = 1
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = 60, 60, 60
 
         # Настройки корабля
         self.ship_speed_factor = 1.5
