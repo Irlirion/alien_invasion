62a217a284bc0d9183d54534938044ce6fdfbbff
Irlirion <43699715+Irlirion@users.noreply.github.com>
2019-02-19 11:07:51 +0300

    Добавил поддержку изменения скорости корабля

--- ship.py
+++ ship.py
@@ -2,9 +2,10 @@ import pygame
 
 
 class Ship():
-    def __init__(self, screen):
+    def __init__(self, ai_settings, screen):
         """Инициализирует корабль и задает его начальную позицию"""
         self.screen = screen
+        self.ai_settings = ai_settings
 
         # Загрузка изображения корабля и получение прямоугольника
         self.image = pygame.image.load('images/ship.bmp')
@@ -15,16 +16,23 @@ class Ship():
         self.rect.centerx = self.screen_rect.centerx
         self.rect.bottom = self.screen_rect.bottom
 
+        # охранение вещественной координаты центра корабля
+        self.center = float(self.rect.centerx)
+
         # Флаги перемещения
         self.moving_right = False
         self.moving_left = False
 
     def update(self):
         """Обновляет позицию корабля с учетом флагов"""
+        # Обновляет атрибут enter, не rect
         if self.moving_right:
-            self.rect.centerx += 1
+            self.center += self.ai_settings.ship_speed_factor
         if self.moving_left:
-            self.rect.centerx -= 1
+            self.center -= self.ai_settings.ship_speed_factor
+
+        # Обновление атрибута rect на основе self.center
+        self.rect.centerx = self.center
 
     def blitme(self):
         """Рисует корабль в текущей позиции"""
