688d9347375385938c043094e172bbb1efa7d699
Irlirion <43699715+Irlirion@users.noreply.github.com>
2019-02-21 20:59:54 +0300

    Скобки у кортежа явно указаны

--- settings.py
+++ settings.py
@@ -6,13 +6,13 @@ class Settings:
         # Параметры экрана
         self.screen_width = 1200
         self.screen_height = 800
-        self.bg_color = 230, 230, 230
+        self.bg_color = (230, 230, 230)
 
         # Параметры пули
         self.bullet_speed_factor = 1
         self.bullet_width = 3
         self.bullet_height = 15
-        self.bullet_color = 60, 60, 60
+        self.bullet_color = (60, 60, 60)
         self.bullets_allowed = 3
 
         # Настройки корабля
