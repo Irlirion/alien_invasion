fa4a5774200abdab30824f194fb3bb3d37d10e53
Irlirion <43699715+Irlirion@users.noreply.github.com>
2019-02-21 14:07:38 +0300

    Добавил переменную ограничения пуль

--- settings.py
+++ settings.py
@@ -13,6 +13,7 @@ class Settings:
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = 60, 60, 60
+        self.bullets_allowed = 3
 
         # Настройки корабля
         self.ship_speed_factor = 1.5
