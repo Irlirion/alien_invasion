edbdc90c90ba93d13d6e475376f921fb04a15a75
Irlirion <43699715+Irlirion@users.noreply.github.com>
2019-02-23 17:05:36 +0300

    Добавил настройки скорости снижения флота и направление движения

--- settings.py
+++ settings.py
@@ -20,3 +20,6 @@ class Settings:
 
         # Настройки пришельцев
         self.alien_speed_factor = 1
+        self.fleet_drop_speed = 10
+        # fleet_direction = 1 бозначает движение вправо, а -1 - влево
+        self.fleet_direction = 1
