3537722c6175203468858e69028393890c91724f
Irlirion <43699715+Irlirion@users.noreply.github.com>
2019-02-23 18:34:39 +0300

    величил скорость пуль

--- settings.py
+++ settings.py
@@ -8,8 +8,8 @@ class Settings:
         self.screen_height = 800
         self.bg_color = (230, 230, 230)
 
-        # Параметры пули
-        self.bullet_speed_factor = 1
+        # Настройки пуль
+        self.bullet_speed_factor = 3
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
