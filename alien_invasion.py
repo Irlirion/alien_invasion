5edd1b169d557136f61b12bed54654699120ccf9
Irlirion <43699715+Irlirion@users.noreply.github.com>
2019-02-26 16:56:51 +0300

    Добавил уменьшение жизней, когда пришельцы доходят до конца экрана Добавил остановку игры

--- alien_invasion.py
+++ alien_invasion.py
@@ -32,10 +32,11 @@ def run_game():
     # Запуск сновного цикла игры
     while True:
         gf.check_events(ai_settings, screen, stats, play_button, ship, bullets)
-        ship.update()
-        gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
-        gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
-        gf.update_screen(ai_settings, screen, stats, ship, aliens, bullets, play_button)
+        if stats.game_active:
+            ship.update()
+            gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
+            gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
+            gf.update_screen(ai_settings, screen, stats, ship, aliens, bullets, play_button)
 
 
 run_game()
--- game_functions.py
+++ game_functions.py
@@ -151,15 +151,28 @@ def change_fleet_direction(ai_settings, aliens):
 
 def ship_hit(ai_settings, stats, screen, ship, aliens, bullets):
     """Обрабатывает столкновение корабля с пришельцем"""
-    # Уменьшение ships_left
-    stats.ships_left -= 1
+    if stats.ships_left > 0:
+        # Уменьшение ships_left
+        stats.ships_left -= 1
 
-    # Очистка списков пришельцев и пуль
-    aliens.empty()
-    ship.center_ship()
+        # Очистка списков пришельцев и пуль
+        aliens.empty()
+        ship.center_ship()
 
-    # Пауза
-    sleep(0.5)
+        # Пауза
+        sleep(0.5)
+    else:
+        stats.game_active = False
+
+
+def check_aliens_bottom(ai_settings, stats, screen, ship, aliens, bullets):
+    """Проверяет, добрались ли пришельцы до нижнего края экрана"""
+    screen_rect = screen.get_rect()
+    for alien in aliens.sprites():
+        if alien.rect.bottom >= screen_rect.bottom:
+            # Происходит то же, что при столкновении с кораблем
+            ship_hit(ai_settings, stats, screen, ship, aliens, bullets)
+            break
 
 
 def update_aliens(ai_settings, stats, screen, ship, aliens, bullets):
@@ -173,3 +186,6 @@ def update_aliens(ai_settings, stats, screen, ship, aliens, bullets):
     # Проверка коллизий "пришелец-корабль"
     if pygame.sprite.spritecollideany(ship, aliens):
         ship_hit(ai_settings, stats, screen, ship, aliens, bullets)
+
+    # Проверка пришельцев, добравшихся до нижнего края экрана
+    check_aliens_bottom(ai_settings, stats, screen, ship, aliens, bullets)
