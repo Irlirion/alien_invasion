2b86cc8bf8d9f6968e782e2e4db2c27f3556dd11
Irlirion <43699715+Irlirion@users.noreply.github.com>
2019-02-27 20:06:24 +0300

    Добавил увеличение очков за пришельца при победе

--- settings.py
+++ settings.py
@@ -22,7 +22,8 @@ class Settings:
 
         # Темп ускорения игры
         self.speedup_scale = 1.2
-
+        # Темп роста стоимости пришельцев
+        self.score_scale = 1.5
         self.initialize_dynamic_settings()
 
     def initialize_dynamic_settings(self):
@@ -38,7 +39,8 @@ class Settings:
         self.alien_points = 50
 
     def increase_speed(self):
-        """Увеличивает настройки скорости"""
+        """Увеличивает настройки скорости и стоимости пришельцев"""
         self.ship_speed_factor *= self.speedup_scale
         self.bullet_speed_factor *= self.speedup_scale
         self.alien_speed_factor *= self.speedup_scale
+        self.alien_points = int(self.alien_points * self.score_scale)
