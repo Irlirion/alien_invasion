0a5f64caeda93d53501fb8ed9d029dd29657e27e
Irlirion <43699715+Irlirion@users.noreply.github.com>
2019-02-27 19:48:17 +0300

    Инициализировал класс очков

--- alien_invasion.py
+++ alien_invasion.py
@@ -4,6 +4,7 @@ from pygame.sprite import Group
 import game_functions as gf
 from button import Button
 from game_stats import GameStats
+from scoreboard import Scoreboard
 from settings import Settings
 from ship import Ship
 
@@ -18,8 +19,9 @@ def run_game():
 
     # Создание кнопки Play
     play_button = Button(ai_settings, screen, "Play")
-    # Создание экземпляра для хранения игровой статистики
+    # Создание экземпляров GameStats и Scoreboard
     stats = GameStats(ai_settings)
+    sb = Scoreboard(ai_settings, screen, stats)
 
     # Создание корабля, группы пуль и группы пришельце
     ship = Ship(ai_settings, screen)
@@ -36,7 +38,7 @@ def run_game():
             ship.update()
             gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
             gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
-        gf.update_screen(ai_settings, screen, stats, ship, aliens, bullets, play_button)
+        gf.update_screen(ai_settings, screen, stats, sb, ship, aliens, bullets, play_button)
 
 
 run_game()
